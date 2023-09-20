14b9e2041b9bedb34fd4964aaa92bf10b469163d
Subrata Das <68939201+Subrata2402@users.noreply.github.com>
2023-09-20 19:27:04 +0000

    update notification

--- background.js
+++ background.js
@@ -1,34 +1,26 @@
 // background.js
 let intervalId = null;
 
-function playAudio() {
+function sendNotification(title, message) {
+	browser.notifications.create({
+		type: "basic",
+		iconUrl: "refresh.png", // Replace with the path to your extension's icon
+		title: title,
+		message: message,
+	});
 	const audio = new Audio('alarm.wav');
 	audio.play();
 }
 
 browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
 	if (message.action === "start") {
-		browser.notifications.create({
-			type: "basic",
-			iconUrl: "refresh.png", // Replace with the path to your extension's icon
-			title: "Reload Started!",
-			message: `The page will reload every ${message.timerValue} seconds.`,
-		});
-
-		playAudio();
+		sendNotification("Reload Started!", `The page will reload every ${message.timerValue} seconds.`);	
 
 		intervalId = setInterval(() => {
 			browser.tabs.reload(message.tabId);
 		}, message.timerValue * 1000);
 	} else if (message.action === "stop") {
-		browser.notifications.create({
-			type: "basic",
-			iconUrl: "refresh.png", // Replace with the path to your extension's icon
-			title: "Reload Stopped!",
-			message: `The page will no longer reload.`,
-		});
-
-		playAudio();
+		sendNotification("Reload Stopped!", `The page will no longer reload.`);
 
 		clearInterval(intervalId);
 	}
@@ -41,14 +33,7 @@ browser.runtime.onConnect.addListener((port) => {
 	port.onMessage.addListener((message) => {
 		if (message.action === 'notify') {
 			// Handle the message from the content script
-			browser.notifications.create({
-				type: "basic",
-				iconUrl: "refresh.png", // Replace with the path to your extension's icon
-				title: "Question posted!",
-				message: "A question has been posted on the Chegg.",
-			});
-
-			playAudio();
+			sendNotification("Question posted!", "A question has been posted on the Chegg.");
 
 			console.log('Received message in background script:', message.data);
 
